11c3f0cf0db699a5ffb15ea4fdf2953fdf6a7290
Farabi002 <152080792+Farabi002@users.noreply.github.com>
2024-08-06 18:15:01 +0600

    Add files via upload

---
+++ streamlit_practice1.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import pandas as pd 
+from PIL import Image
+
+
+
+# Load the logo image
+logo = Image.open("download.jpeg")
+# Display the logo image
+st.image(logo, width=100)
+
+
+st.header('Welcome !!')
+
+
+df = pd.read_excel(r'C:\Users\FARABI\Desktop\Webapp\BME_401_CT_ Marksheet.xlsx')
+
+# Text input field
+student_id = pd.to_numeric(st.text_input("Enter Student ID:"), errors='coerce')
+
+
+button_clicked = st.button('Submit')
+
+
+if button_clicked:
+    if student_id in df['Student ID'].values:
+        # Find the Total Marks for the given Student ID
+        total_marks = df.loc[df['Student ID'] == student_id, 'Total Marks'].values[0]
+        if total_marks >= 12:
+            with st.expander("Result", expanded=True):
+             st.write(f'Total Marks: {total_marks} (passed)')
+        else:
+           with st.expander("Result", expanded=True):
+             st.write(f'Total Marks: {total_marks} (failed)')
+    else: 
+        st.write('Wrong Student ID')
+
+else: 
+   pass
+    
+
+     
+
